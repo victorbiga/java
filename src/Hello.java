5cc168fc88345aeccdc9a363699f333103da8e35
Victor Biga <victor.biga@gmail.com>
2021-09-10 19:13:33 +0100

    variables and expressions

--- src/Hello.java
+++ src/Hello.java
@@ -1,9 +1,11 @@
 public class Hello {
-
     public static void main(String[] args) {
         System.out.println("Hello, Victor");
+        int myFirstNumber = (10 + 5) + (2 *10);
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
 
-        int myFirstNumber = (10 + 5) + (2 * 10);
-        System.out.println(myFirstNumber);
     }
 }
