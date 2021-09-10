e62b91a791ec4b809445c3d54a1248dfe1ed45b6
Victor Biga <victor.biga@gmail.com>
2021-09-10 19:29:16 +0100

    Int wrapper values

--- ByteShortIntLong/src/com/treidbot/Main.java
+++ ByteShortIntLong/src/com/treidbot/Main.java
@@ -3,6 +3,14 @@ package com.treidbot;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int myValue = 10000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum Value = " + myMinIntValue);
+        System.out.println("Integer Maximum Value = " + myMaxIntValue);
+
+
     }
 }
