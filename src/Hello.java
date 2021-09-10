f9d5314a0e6a3d1f4c00f0cc616010414cb730fc
Victor Biga <victor.biga@gmail.com>
2021-09-10 19:17:25 +0100

    using var with var for value

--- src/Hello.java
+++ src/Hello.java
@@ -1,10 +1,12 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, Victor");
+
         int myFirstNumber = (10 + 5) + (2 *10);
         int mySecondNumber = 12;
-        int myThirdNumber = 6;
+        int myThirdNumber = myFirstNumber * 2;
         int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
         System.out.println(myTotal);
 
     }
