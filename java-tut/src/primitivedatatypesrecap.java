6ee7e596d157b417cfad4f66f830abc4e138650a
VictorBiga <victor.biga@gmail.com>
2021-09-17 09:55:38 +0100

    types with copyright symbol

---
+++ java-tut/src/primitivedatatypesrecap.java
@@ -0,0 +1,24 @@
+public class primitivedatatypesrecap {
+    public static void main(String[] args) {
+        // byte
+        // short
+        // int
+        // long
+        // float
+        // double
+        // char
+        // boolean
+
+        String myString = "This is a string";
+        System.out.println("myString is equal to " + myString);
+        myString = myString + ", and this is more.";
+        System.out.println("myString is equal to " + myString);
+        myString = myString + ", and this is even more than previously";
+        System.out.println(myString);
+        myString = myString + " \u00A9 2021";
+        System.out.println(myString);
+
+
+
+    }
+}
