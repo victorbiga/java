98308ee2af105e9622669bb9b6334face1ac012c
VictorBiga <victor.biga@gmail.com>
2021-09-17 16:01:36 +0100

    Strings additon

--- java-tut/src/primitivedatatypesrecap.java
+++ java-tut/src/primitivedatatypesrecap.java
@@ -18,7 +18,17 @@ public class primitivedatatypesrecap {
         myString = myString + " \u00A9 2021";
         System.out.println(myString);
 
+        String numberString = "250.55";
+        numberString = numberString + "49.95";
+        System.out.println(numberString);
 
+        String lastString = "10";
+        int myInt = 50;
+        lastString = lastString + myInt;
+        System.out.println(lastString);
 
+        double doubleNumber = 120.47d;
+        lastString = lastString + doubleNumber;
+        System.out.println("LastString is equal to " + lastString);
     }
 }
